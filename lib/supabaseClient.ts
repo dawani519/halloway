b9b3dc078b2543660d249c7a108b975629a05cf6
dawani519 <danielamos519@gmail.com>
2025-02-24 23:29:29 +0100

    Updated login and registration with Supabase auth

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be provided!");
+}
 
 export const createSupabaseBrowserClient = () => {
   return createBrowserClient(supabaseUrl, supabaseAnonKey);
